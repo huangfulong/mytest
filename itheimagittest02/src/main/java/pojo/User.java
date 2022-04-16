6c44ae43a43d90a6cdc005d8440e0a07468e2106
hfl <1032780686@qq.com>
2022-04-16 23:25:20 +0800

    晚上提交

--- itheimagittest02/src/main/java/pojo/User.java
+++ itheimagittest02/src/main/java/pojo/User.java
@@ -4,6 +4,15 @@ public class User {
     private String userName;
     private String password;
     private String email;
+    private String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
     public String getEmail() {
         return email;
