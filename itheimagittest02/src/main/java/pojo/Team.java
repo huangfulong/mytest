55fd8c1a757c1fa447ec0ea5190a809962705f3c
hfl <1032780686@qq.com>
2022-04-16 23:51:07 +0800

    晚上提交dev5

--- itheimagittest02/src/main/java/pojo/Team.java
+++ itheimagittest02/src/main/java/pojo/Team.java
@@ -3,4 +3,20 @@ package pojo;
 public class Team {
     private String teamName;
     private String teamPassword;
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public String getTeamPassword() {
+        return teamPassword;
+    }
+
+    public void setTeamPassword(String teamPassword) {
+        this.teamPassword = teamPassword;
+    }
 }
